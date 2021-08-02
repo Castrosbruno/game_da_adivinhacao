8a44965eb968686de0f66deb29de8f56723c5dce
castrosbruno <bruno.2606@hotmail.com>
2021-08-02 19:51:03 -0300

    Atualização do game, quanto a perf

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -19,8 +19,7 @@ def pede_dificuldade
   puts "Qual o nível de dificuldade?"
   puts "(1) Muito fácil (2) Fácil (3) Normal (4) Difícil (5) Impossível"
   puts "Escolha: "
-  dificuldade = gets.to_i
-
+  return gets.to_i
 end
 
 def sorteia_numero_secreto(dificuldade)
@@ -29,14 +28,14 @@ def sorteia_numero_secreto(dificuldade)
   when 1
     maximo = 30
   when 2
-     maximo = 60
+    maximo = 60
   when 3
     maximo = 100
   when 4
     maximo = 150
-        else
-          maximo = 200
-          end
+  else
+    maximo = 200
+  end
   puts "\nEscolhendo um número secreto entre 1 e #{maximo}\n\n"
   sorteado = rand(maximo) + 1
   puts "Escolhido... que tal adivinhar hoje nosso número secreto?\n\n"
@@ -52,7 +51,7 @@ def pede_um_numero(chutes, tentativa, limite_de_tentativas)
   chute.to_i
 end
 
-def verifica_se_acertou(numero_secreto,chute)
+def verifica_se_acertou(numero_secreto, chute)
   acertou = numero_secreto == chute.to_i
 
   if acertou
@@ -62,11 +61,11 @@ def verifica_se_acertou(numero_secreto,chute)
 
   maior = numero_secreto > chute.to_i
 
-    if maior
-      puts "O número secreto é maior!"
-    else
-      puts "O número secreto é menor!"
-    end
+  if maior
+    puts "O número secreto é maior!"
+  else
+    puts "O número secreto é menor!"
+  end
   return false
 end
 
@@ -98,15 +97,13 @@ def joga(nome, dificuldade)
   limite_de_tentativas = 5
   chutes = []
 
-
   for tentativa in 1..limite_de_tentativas
 
     chute = pede_um_numero chutes, tentativa, limite_de_tentativas
     chutes << chute
 
-    if nome == "Bruno"
-      ganhou
-      break
+    if nome.upcase == "BRUNO"
+      numero_secreto = chute
     end
     pontos_a_perder = (chute - numero_secreto).abs / 2.0
     pontos_ate_agora -= pontos_a_perder
@@ -116,12 +113,13 @@ def joga(nome, dificuldade)
   end
 
   puts "Você ganhou #{pontos_ate_agora} pontos."
+  puts "O número correto era #{numero_secreto}"
 end
 
-def nao_quer_jogar?
+def nao_quer_jogar
   puts "Deseja jogar novamente? (S/N)"
   quero_jogar = gets.strip
-  nao_quero_jogar = quero_jogar.upcase == "N"
+  return quero_jogar.upcase == "N"
 end
 
 nome = dar_boas_vindas
@@ -129,7 +127,7 @@ dificuldade = pede_dificuldade
 
 loop do
   joga nome, dificuldade
-  if nao_quer_jogar?
-    break
+  if nao_quer_jogar
+       break
   end
 end
